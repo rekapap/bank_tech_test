628ef473c3e4aabc026c77b0d95459f53b9f4bd4
Reka <rekapap28@gmail.com>
2018-12-03 20:45:18 +0000

    adds withdrawal

--- lib/account.rb
+++ lib/account.rb
@@ -11,4 +11,8 @@ class Account
   def deposit(amount:)
     @balance += amount
   end
+
+  def withdraw(amount:)
+    @balance -= amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,9 @@ require 'account'
 
 describe Account do
   subject { described_class.new }
+  before(:all) do
+    AMOUNT = 1000
+  end
 
   describe '#balance' do
     it 'has zero as an initial balance' do
@@ -11,18 +14,25 @@ describe Account do
 
   describe '#deposit' do
     it 'adds money to the balance' do
-      AMOUNT = 1000
       expect { subject.deposit(amount: AMOUNT) }.to change {
         subject.balance
       }.from(0).to(AMOUNT)
     end
 
     it 'can add money to the balance multiple times' do
-      AMOUNT = 1000
       subject.deposit(amount: AMOUNT)
       expect { subject.deposit(amount: AMOUNT) }.to change {
         subject.balance
       }.from(AMOUNT).to(AMOUNT + AMOUNT)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'withdraw money from the balance' do
+      subject.deposit(amount: AMOUNT)
+      expect { subject.withdraw(amount: AMOUNT) }.to change {
+        subject.balance
+      }.from(AMOUNT).to(0)
     end
   end
 end
