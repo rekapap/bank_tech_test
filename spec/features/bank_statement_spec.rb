5b73e58f57a3f55a28627e68f0ecc93dca4bec0b
Reka <rekapap28@gmail.com>
2018-12-04 21:48:17 +0000

    changing string quotes and indentation in feature test

--- spec/features/bank_statement_spec.rb
+++ spec/features/bank_statement_spec.rb
@@ -6,10 +6,10 @@ feature 'Bank statement' do
   context 'after deposits and withdrawals' do
     scenario 'user can see his balance, withdrawals and deposits with date' do
       bank_statement =
-      "   date    ||  credit ||  debit || balance\n" \
+        "   date    ||  credit ||  debit || balance\n" \
       "14/01/2012 ||         || 500.00 || 2500.00\n" \
       "13/01/2012 || 2000.00 ||        || 3000.00\n" \
-      "10/01/2012 || 1000.00 ||        || 1000.00"
+      '10/01/2012 || 1000.00 ||        || 1000.00'
       account = Account.new
       account.deposit(amount: 1000, date: '10-01-2012')
       account.deposit(amount: 2000, date: '13-01-2012')
