497a6ac4aa4e176d8d478ab5225ca4a8e560dc10
Reka <rekapap28@gmail.com>
2018-12-04 21:38:44 +0000

    implemented feature test

--- spec/features/bank_statement_spec.rb
+++ spec/features/bank_statement_spec.rb
@@ -1,14 +1,15 @@
 require_relative '../spec_helper'
 require 'account'
-require 'statementprinter'
+require 'statement_printer'
 
 feature 'Bank statement' do
   context 'after deposits and withdrawals' do
     scenario 'user can see his balance, withdrawals and deposits with date' do
-      bank_statement = 'date       || credit  || debit  || balance' \
-      '14/01/2012 ||         || 500.00 || 2500.00' \
-      '13/01/2012 || 2000.00 ||        || 3000.00' \
-      '10/01/2012 || 1000.00 ||        || 1000.00'
+      bank_statement =
+      "   date    ||  credit ||  debit || balance\n" \
+      "14/01/2012 ||         || 500.00 || 2500.00\n" \
+      "13/01/2012 || 2000.00 ||        || 3000.00\n" \
+      "10/01/2012 || 1000.00 ||        || 1000.00"
       account = Account.new
       account.deposit(amount: 1000, date: '10-01-2012')
       account.deposit(amount: 2000, date: '13-01-2012')
