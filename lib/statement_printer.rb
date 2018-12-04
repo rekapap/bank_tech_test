3d96b8b84992a101f1811cc33c2665d748c90d89
Reka <rekapap28@gmail.com>
2018-12-04 20:44:43 +0000

    statement printer can do  deposit with header

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -3,6 +3,33 @@ class StatementPrinter
   HEADER = '   date    ||  credit ||  debit || balance'.freeze
 
   def self.print(transactionlog)
-    return HEADER if transactionlog.data.empty?
+    log = transactionlog.data
+    return HEADER if log.empty?
+
+    record_log = log.map do |record|
+      make_string_record(record)
+    end
+    HEADER + "\n" + record_log.join("\n")
+  end
+
+  private_class_method
+
+  def self.make_string_record(record)
+    date = date_format(record[:date])
+    transaction = transaction_string(record[:amount])
+    date + ' || ' + transaction + ' ||'
+  end
+
+  def self.transaction_string(amount)
+    amount_string = amount_format(amount)
+    amount_string + ' ||       '
+  end
+
+  def self.amount_format(amount)
+    format('%.2f', amount)
+  end
+
+  def self.date_format(date)
+    date.strftime('%d/%m/%Y')
   end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -2,7 +2,6 @@ require 'statement_printer'
 
 describe StatementPrinter do
   describe '.print' do
-
     context 'empty recorde' do
       let(:transactionlog_empty) { double :transactionlog, data: [] }
       it 'returns the header if there is no record in the log' do
@@ -12,6 +11,20 @@ describe StatementPrinter do
       it 'calls the data method on transactionlog' do
         expect(transactionlog_empty).to receive(:data)
         described_class.print(transactionlog_empty)
+      end
+    end
+
+    context 'not empty record' do
+      context 'desposit records only' do
+        let(:bank_statement) do
+          "   date    ||  credit ||  debit || balance\n" \
+          '01/01/2018 || 1000.00 ||        ||'
+        end
+        let(:array) { [{ amount: AMOUNT, date: Date.parse(DATE) }] }
+        let(:transactionlog) { double :transactionlog, data: array }
+        it 'can print out the date and the debit' do
+          expect(described_class.print(transactionlog)).to eq(bank_statement)
+        end
       end
     end
   end
